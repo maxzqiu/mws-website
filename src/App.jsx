595a44865d9cb693b5c9f7e4dc15fd782b152c9e
Max Qiu <maxzqiu@gmail.com>
2025-02-01 12:55:49 -0800

    added presentations

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 
+export function Presentations(){
+  return (
+    <>
+      <h2>MWS Presentations</h2>
+      <div className="presentations">
+        <button><Link to="/presentations/santa-ana-winds-in-southern-california">
+        <div>
+          <p>Santa Ana Winds in Southern California and Analysis on the Jan 7-8, 2025 Santa Ana Wind Event That Fueled The Devastating LA Wildfires</p>
+          <img width="500px" src="santa-ana-winds-poster.png" alt="Santa Ana Winds Presentation Poster"></img>
+        </div>
+        </Link></button>
+      </div>
+      
+    </>
+  )
+}
+function CreatePage({title, name}){
+  return (
+    <>
+    <h2>{title}</h2>
+    <embed
+            src={name}
+            type="application/pdf"
+            className="embed"
+            
+            >
+    </embed>
+    </>
+  )
+}
+
 export function About(){
   return (
     <>
@@ -42,7 +73,9 @@ export function NavBar(){
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
+          <li><Link to="/presentations">MWS Presentations</Link></li>
           <li><Link to="/about">About MWS</Link></li>
+          
         </ul>
       </nav>
     </>
@@ -71,6 +104,7 @@ function App() {
         </div>
         
         <h4>Welcome to our website! </h4>
+        
         <br></br>
         <label htmlFor="time"><b>CURRENT TIME</b></label>
         <table id="time">
@@ -93,6 +127,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="/presentations" element={<Presentations />}></Route>
+          <Route path="/presentations/santa-ana-winds-in-southern-california" element={<CreatePage 
+          title="Santa Ana Winds in Southern California and Analysis on the Jan 7-8, 2025 Santa Ana Wind Event That Fueled The Devastating LA Wildfires"
+          name="/public/Santa-Ana-Winds.pdf"
+           />}></Route>
         </Routes>
         <br></br>
         <br></br>
